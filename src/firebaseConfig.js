e1fcc9f20117af566737d36f98ff6f8411060b7e
Rishabh Pandey <rishabhpandey2963@gmail.com>
2025-02-08 02:05:11 +0530

    more security

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: "namasteayodhya-62749.firebaseapp.com",
-    projectId: "namasteayodhya-62749",
-    storageBucket: "namasteayodhya-62749.firebasestorage.app",
-    messagingSenderId: "552672421326",
-    appId: "1:552672421326:web:7a03529a983da38ad9f79f",
-    measurementId: "G-VDN28P7J5Q"
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE,
+    messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGE,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId:import.meta.env.VITE_FIREBASE_MEASURMENT,
   };
   
 const app = initializeApp(firebaseConfig);
